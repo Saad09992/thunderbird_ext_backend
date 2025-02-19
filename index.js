24d9afd74fc5cd6e6aeff8a41e30f5f2daeac70d
M~Saad <saadofficial0999@gmail.com>
2025-02-19 13:18:01 +0500

    cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 // });
 dotenv.config();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
