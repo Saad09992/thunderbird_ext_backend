3ec38c680682f41a857c10f2e387993c38e135ce
M~Saad <saadofficial0999@gmail.com>
2025-02-19 13:38:23 +0500

    cors update

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ const app = express();
 // });
 dotenv.config();
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
