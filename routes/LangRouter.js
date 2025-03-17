41263cb8625d657ee221bc58efc09d315a5e79cc
M Saad <143897969+Saad09992@users.noreply.github.com>
2025-03-17 22:47:30 +0500

    remove dataset

--- routes/LangRouter.js
+++ routes/LangRouter.js
@@ -5,13 +5,36 @@ import {
   getDataFromFile,
   uploadWritingStyle,
   uploadDataset,
+  removeDataset,
 } from "../controller/LangController.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import multer from "multer";
+
+const __filename = fileURLToPath(import.meta.url);
+const currentDirname = path.dirname(__filename);
+const rootDir = path.resolve(currentDirname, "../");
+
+const imageStorage = multer.diskStorage({
+  destination: path.join(rootDir, "/public/datasets"),
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.originalname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({
+  storage: imageStorage,
+});
+
 const langRouter = Router();
 
 langRouter.post("/api/process-emails", processEmails);
 langRouter.post("/api/register-session", registerSession);
 langRouter.post("/api/get-data-from-file", getDataFromFile);
 langRouter.post("/api/upload-writing-style", uploadWritingStyle);
-langRouter.post("/api/upload-dataset",uploadDataset);
-
+langRouter.post("/api/upload-dataset", uploadDataset);
+langRouter.post("/api/remove-dataset",removeDataset);
 export default langRouter;
